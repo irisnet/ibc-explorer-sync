04b51b3dfcf46cc36f38a055df0818a73541cfa5
huangweichang <weichang@bianjie.ai>
2025-09-04 14:01:43 +0800

    feat: Initial commit

---
+++ libs/msgparser/modules/common.go
@@ -0,0 +1,56 @@
+package msgs
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	cdc "github.com/irisnet/ibc-explorer-sync/libs/msgparser/codec"
+	models "github.com/irisnet/ibc-explorer-sync/libs/msgparser/types"
+	"github.com/irisnet/ibc-explorer-sync/libs/msgparser/utils"
+)
+
+func CreateMsgDocInfo(msg sdk.Msg, handler func() (Msg, []string)) MsgDocInfo {
+	var (
+		docTxMsg models.TxMsg
+		signers  []string
+		addrs    []string
+	)
+
+	m, addrcollections := handler()
+
+	m.BuildMsg(msg)
+	docTxMsg = models.TxMsg{
+		Type: m.GetType(),
+		Msg:  m,
+	}
+
+	_, signers = models.BuildDocSigners(msg.GetSigners())
+	addrs = append(addrs, signers...)
+	addrs = append(addrs, addrcollections...)
+
+	return MsgDocInfo{
+		DocTxMsg: docTxMsg,
+		Signers:  signers,
+		Addrs:    addrs,
+	}
+}
+
+// ConvertMsg
+// Deprecated, use type assertion instead of this
+func ConvertMsg(v interface{}, msg interface{}) {
+	utils.UnMarshalJsonIgnoreErr(utils.MarshalJsonIgnoreErr(v), &msg)
+}
+
+func ConvertAny(v *types.Any) string {
+	if v != nil {
+		return utils.MarshalJsonIgnoreErr(v)
+	}
+	return ""
+}
+
+func UnmarshalAcknowledgement(bytesdata []byte) string {
+	var result Acknowledgement
+	if err := cdc.GetMarshaler().UnmarshalJSON(bytesdata, &result); err != nil {
+		return string(bytesdata)
+	}
+	return result.String()
+}
